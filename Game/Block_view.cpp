768a4d7094009f8da28cf11dac31c8a6d7e81b14
egor79k <egor79k@yandex.ru>
2020-05-04 10:15:41 +0300

    Auto commiting

--- Game/Block_view.cpp
+++ Game/Block_view.cpp
@@ -1,103 +1,45 @@
 #include <iostream>
 #include "Blocks.h"
 
+
+void Print_CT (int color_0, int back_0)
+{
+	for (int type = 0; type < 3; type += 2)
+	{
+		for (int color = color_0; color < 100; color += 60)
+		{
+			for (int back = back_0; back < 100; back += 60)
+			{
+				printf("\t\e[%d;%d;%dm\U00002591\U00002591\e[0m", type, color, back);
+				printf("\e[%d;%d;%dm\U00002592\U00002592\e[0m", type, color, back);
+				printf("\e[%d;%d;%dm\U00002593\U00002593\e[0m", type, color, back);
+				printf("\e[%d;%d;%dm\U00002588\U00002588\e[0m", type, color, back);
+
+				printf(" \\e[%d;%d;%dm\\U00002591", type, color, back);
+				printf(" \\e[%d;%d;%dm\\U00002592", type, color, back);
+				printf(" \\e[%d;%d;%dm\\U00002593", type, color, back);
+				printf(" \\e[%d;%d;%dm\\U00002588", type, color, back);
+			}
+			putchar ('\n');
+		}
+	}
+	putchar ('\n');
+}
+
+
 int main ()
 {
-	/*for (int i = 0; i <= 140; ++i)
+	for (int i = 0; i < 8; ++i)
 	{
-		printf("\e[0m%3d - %s\n", i, BLOCKS[i]);
-	}*/
-	//::::ORANGE::::
-	for (int type = 0; type < 3; type += 2)
-	{
-		for (int color = 33; color < 100; color += 60)
+		for (int j = 0; j < 8; ++j)
 		{
-			for (int back = 41; back < 100; back += 60)
+			if (j != i)
 			{
-				printf("\t\e[%d;%d;%dm\U00002591\U00002591\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002592\U00002592\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002593\U00002593\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002588\U00002588\e[0m", type, color, back);
+				Print_CT (30 + i, 40 + j);
+				Print_CT (30 + j, 40 + i);
 			}
-			printf("\n");
 		}
 	}
 
-	for (int type = 0; type < 3; type += 2)
-	{
-		for (int color = 31; color < 100; color += 60)
-		{
-			for (int back = 43; back < 100; back += 60)
-			{
-				printf("\t\e[%d;%d;%dm\U00002591\U00002591\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002592\U00002592\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002593\U00002593\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002588\U00002588\e[0m", type, color, back);
-				printf("\n");
-			}
-		}
-	}
-	//::::BROWN::::
-	printf("\n");
-	for (int type = 0; type < 3; type += 2)
-	{
-		for (int color = 32; color < 100; color += 60)
-		{
-			for (int back = 41; back < 100; back += 60)
-			{
-				printf("\t\e[%d;%d;%dm\U00002591\U00002591\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002592\U00002592\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002593\U00002593\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002588\U00002588\e[0m", type, color, back);
-			}
-			printf("\n");
-		}
-	}
-
-	for (int type = 0; type < 3; type += 2)
-	{
-		for (int color = 31; color < 100; color += 60)
-		{
-			for (int back = 42; back < 100; back += 60)
-			{
-				printf("\t\e[%d;%d;%dm\U00002591\U00002591\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002592\U00002592\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002593\U00002593\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002588\U00002588\e[0m", type, color, back);
-				printf("\n");
-			}
-		}
-	}
-	//::::
-	printf("\n");
-	for (int type = 0; type < 3; type += 2)
-	{
-		for (int color = 34; color < 100; color += 60)
-		{
-			for (int back = 43; back < 100; back += 60)
-			{
-				printf("\t\e[%d;%d;%dm\U00002591\U00002591\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002592\U00002592\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002593\U00002593\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002588\U00002588\e[0m", type, color, back);
-			}
-			printf("\n");
-		}
-	}
-
-	for (int type = 0; type < 3; type += 2)
-	{
-		for (int color = 33; color < 100; color += 60)
-		{
-			for (int back = 44; back < 100; back += 60)
-			{
-				printf("\t\e[%d;%d;%dm\U00002591\U00002591\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002592\U00002592\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002593\U00002593\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002588\U00002588\e[0m", type, color, back);
-				printf("\n");
-			}
-		}
-	}
 	return 0;
 }
