c8369ac2347153349d0968cd47651506963f6ac3
egor79k <egor79k@yandex.ru>
2020-05-04 10:50:11 +0300

    Auto commiting

--- Game/Block_view.cpp
+++ Game/Block_view.cpp
@@ -10,15 +10,12 @@ void Print_CT (int color_0, int back_0)
 		{
 			for (int back = back_0; back < 100; back += 60)
 			{
-				printf("\t\e[%d;%d;%dm\U00002591\U00002591\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002592\U00002592\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002593\U00002593\e[0m", type, color, back);
-				printf("\e[%d;%d;%dm\U00002588\U00002588\e[0m", type, color, back);
+				printf("\t\e[%d;%d;%dm\U00002591\U00002591", type, color, back);
+				printf("\U00002592\U00002592");
+				printf("\U00002593\U00002593");
+				printf("\U00002588\U00002588\e[0m");
 
-				printf(" \\e[%d;%d;%dm\\U00002591", type, color, back);
-				printf(" \\e[%d;%d;%dm\\U00002592", type, color, back);
-				printf(" \\e[%d;%d;%dm\\U00002593", type, color, back);
-				printf(" \\e[%d;%d;%dm\\U00002588", type, color, back);
+				printf(" \\e[%d;%d;%dm \\U00002591 \\U00002592 \\U00002593 \\U00002588", type, color, back);
 			}
 			putchar ('\n');
 		}
@@ -34,9 +31,9 @@ int main ()
 		{
 			if (j != i)
 			{
-				printf(" \e[%dm\U00002588\e[0m + \e[%dm \e[0m\n", 30 + i, 40 + j);
+				printf("\e[%dm\U00002588\e[0m+\e[%dm \e[0m = \e[%d;%dmA\e[0m\n", 30 + i, 40 + j, 30 + i, 40 + j);
 				Print_CT (30 + i, 40 + j);
-				printf(" \e[%dm\U00002588\e[0m + \e[%dm \e[0m\n", 30 + j, 40 + i);
+				printf("\e[%dm\U00002588\e[0m+\e[%dm \e[0m = \e[%d;%dmA\e[0m\n", 30 + j, 40 + i, 30 + j, 40 + i);
 				Print_CT (30 + j, 40 + i);
 			}
 		}
