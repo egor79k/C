23ff220962ddf1d0304562af2c4a9d0dd171bd50
egor79k <egor79k@yandex.ru>
2020-05-11 17:44:44 +0300

    Auto commiting

--- Hash_table_class/HT_optimisation/4_Hash_asm_opt/main.cpp
+++ Hash_table_class/HT_optimisation/4_Hash_asm_opt/main.cpp
@@ -14,11 +14,13 @@ const int WORDS_NUM = 1278241;//1152320;//828725;//562433;
 *
 *	return String hash
 */
-#define mmix(h,k) { k *= m; k ^= k >> r; k *= m; h *= m; h ^= k; }
-const unsigned int MUR_SEED = 42764812;
+//#define mmix(h,k) { k *= m; k ^= k >> r; k *= m; h *= m; h ^= k; }
+//const unsigned int MUR_SEED = 42764812;
 
 unsigned int MurmurHash (char *string)
 {
+	//printf("counting...\n");
+	//fflush (stdout);
 	unsigned int hash = 0;
 
 	asm (".intel_syntax noprefix\n"
@@ -30,7 +32,7 @@ unsigned int MurmurHash (char *string)
 // esi == string
 // eax == hash
 
-		"	mov		r8b, ecx			\n"	// unsigned int len = l;
+		"	mov		edi, ecx			\n"	// unsigned int len = l;
 		"	mov		eax, 42764812		\n"	// unsigned int h = MUR_SEED;
 		"	xor		ebx, ebx			\n"	// unsigned int k = 0;
  
@@ -46,6 +48,9 @@ unsigned int MurmurHash (char *string)
 		"	shr     edx, 24				\n"
 		"	xor     ebx, edx			\n"
 		"	imul    ebx, 0x5BD1E995 	\n"
+		"	imul	eax, 0x5BD1E995 	\n"
+		"	mov		edx, ebx			\n"
+		"	xor		eax, edx			\n"
 
 		"	add     rsi, 4				\n"	// string += 4;
 		"	sub     ecx, 4				\n"	// len -= 4;
@@ -55,40 +60,60 @@ unsigned int MurmurHash (char *string)
 
 		"	xor		ebx, ebx			\n"	// unsigned int t = 0;
 
-		"	cmp     ebx, 3\n"
-		"	jne     .loc_tail_2\n"
-		"	mov     eax, byte[rsi+2]\n"
-		"	shl     eax, 16\n"
-		"	xor     ecx, eax\n"
+		"	cmp     ecx, 3				\n"
+		"	je      .length_3			\n"
+		"	cmp     ecx, 2				\n"
+		"	je      .length_2			\n"
+		"	cmp     ecx, 1				\n"
+		"	je      .length_1			\n"
 
-		".loc_tail_2:		\n"
-		"	cmp     ebx, 2\n"
-		"	jne     .loc_tail_1\n"
-		"	mov     eax, byte[rsi+1]\n"
-		"	shl     eax, 8\n"
-		"	xor     ecx, eax\n"
+		".length_3:						\n"
+		"	mov     edx, byte[rsi+2]\n"
+		"	shl     edx, 16\n"
+		"	xor     ebx, edx\n"
 
-		".loc_tail_1:		\n"
-		"	mov   eax, byte[rsi]\n"
-		"	xor     ecx, eax\n"
-		"	imul    ecx, 0x5BD1E995\n"
+		".length_2:						\n"
+		"	mov     edx, byte[rsi+1]\n"
+		"	shl     edx, 8\n"
+		"	xor     ebx, edx\n"
+
+		".length_1:						\n"
+		"	mov   edx, byte[rsi]\n"
+		"	xor     ebx, edx\n"
+ 		
+		"	imul    ebx, 0x5BD1E995 	\n" // mmix (h, t);
+		"	mov     edx, ebx			\n"
+		"	shr     edx, 24				\n"
+		"	xor     ebx, edx			\n"
+		"	imul    ebx, 0x5BD1E995 	\n"
+		"	imul	eax, 0x5BD1E995 	\n"
+		"	mov		edx, ebx			\n"
+		"	xor		eax, edx			\n"
+
+		"	imul    edi, 0x5BD1E995 	\n" // mmix (h, l);
+		"	mov     edx, edi			\n"
+		"	shr     edx, 24				\n"
+		"	xor     edi, edx			\n"
+		"	imul    edi, 0x5BD1E995 	\n"
+		"	imul	eax, 0x5BD1E995 	\n"
+		"	mov		edx, edi			\n"
+		"	xor		eax, edx			\n"
+		
+		"	mov     edx, eax\n"
+		"	shr     edx, 13\n"
+		"	xor     eax, edx\n"
  
-		".loc_finish:		\n"
-		"	mov     eax, ecx\n"
-		"	shr     eax, 13\n"
-		"	xor     ecx, eax\n"
+		"	imul    eax, 0x5BD1E995\n"
  
-		"	imul    ecx, 0x5BD1E995\n"
- 
-		"	mov     eax, ecx\n"
-		"	shr     eax, 15\n"
-		"	xor     eax, ecx\n"
+		"	mov     edx, eax\n"
+		"	shr     edx, 15\n"
+		"	xor     eax, edx\n"
 
 		".att_syntax prefix\n"
  
  		: "=a" (hash)
  		: "S" (string), "c" (strlen (string))
-		: "rcx", "rdx", "rdi"
+		: "rbx", "rdx", "r8"
 		);
 
 /*
@@ -127,6 +152,48 @@ unsigned int MurmurHash (char *string)
 
 	return h;*/
 	return hash;
+}
+
+
+#define mmix(h,k) { k *= m; k ^= k >> r; k *= m; h *= m; h ^= k; }
+const unsigned int MUR_SEED = 42764812;
+
+unsigned int MurmurHash_std (char *string)
+{
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
+	unsigned int l = strlen (string);
+	unsigned int len = l;
+	unsigned int h = MUR_SEED;
+	unsigned int k = 0;
+
+	while (len >= 4)
+	{
+		k = *(unsigned int*)string;
+
+		mmix(h,k);
+
+		string += 4;
+		len -= 4;
+	}
+
+	unsigned int t = 0;
+
+	switch (len)
+	{
+	case 3: t ^= string[2] << 16;
+	case 2: t ^= string[1] << 8;
+	case 1: t ^= string[0];
+	};
+
+	mmix(h,t);
+	mmix(h,l);
+
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return h;
 }
 
 
@@ -174,7 +241,10 @@ void Find_in_HT (HT &HshTb, char *buffer)
 	{
 		if (*buffer == '\0')
 		{
-			//printf("%s\n", HshTb.Find (tmp));
+			//printf("finding %s...\n", tmp);
+			//fflush (stdout);
+			//printf("%p\n", HshTb.Find (tmp));
+			//fflush (stdout);
 			HshTb.Find (tmp);
 			tmp = buffer + 1;
 			count++;
@@ -188,14 +258,17 @@ void Find_in_HT (HT &HshTb, char *buffer)
 
 int main ()
 {
+	//printf("Genoa hash = %u std = %u\n", MurmurHash ("Genoa"), MurmurHash_std ("Genoa"));
+	//printf("and hash = %u std = %u\n", MurmurHash ("and"), MurmurHash_std ("and"));
+	//printf("started hash = %u std = %u\n", MurmurHash ("started"), MurmurHash_std ("started"));
+
 	int count = 0;
 	HT HshTb (HT_SIZE, MurmurHash);
-
 
 	char *buffer = GetBuffer (input_file);			//Filling HT with words from input file
 	Fill_HT (HshTb, buffer);
 
-	//HshTb.Print_lists_length ("stat.csv");
+//	HshTb.Print_lists_length ("stat.csv");
 
 	char *find_buff = GetBuffer (input_find_file);	//Reading find buffer from file
 	char *find_buff_start = find_buff;
