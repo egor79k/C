18fad70a73a0fb269cd5a3975b37567702fb3bb8
egor79k <egor79k@yandex.ru>
2020-05-13 23:40:49 +0300

    Auto commiting

--- AlphaBlend/main.cpp
+++ AlphaBlend/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 
 
-struct BitMapFileHeader
-{
-	uint16_t Type       = 0;
-	uint32_t FileSize   = 0;
-	uint16_t Reserved1  = 0;
-	uint16_t Reserved2  = 0;
-	uint32_t OffsetBits = 0;
-};
+const char *Input_file = "AskhatCat.bmp";
 
-struct BitMapInfoHeader
+
+struct BitMapHeader
 {
+	uint16_t Type            = 0;
+	uint32_t FileSize        = 0;
+	uint16_t Reserved1       = 0;
+	uint16_t Reserved2       = 0;
+	uint32_t OffsetBits      = 0;
 	uint32_t InfoHeaderSize  = 0;
 	uint32_t Width           = 0;
 	uint32_t Height          = 0;
@@ -28,28 +27,35 @@ struct BitMapInfoHeader
 
 class BMP
 {
+
+
+
+
+
+
+
+
 private:
 	char *Image;
-	BitMapFileHeader FileHeader;
-	BitMapInfoHeader InfoHeader;
+	BitMapHeader Header;
 
 public:
-	BMP (char *FileName);
+	BMP (const char *FileName);
 	~BMP ();
 
-	ImageInfo ();
+	void ViewImageInfo ();
 
 };
 
 
-BMP::BMP (cahr *FileName);
+BMP::BMP (const char *FileName)
 {
 	FILE *input = fopen (FileName, "rb");
-	if (input != NULL) printf("Error: Can't open file \"%s\"\n", FileName);
+
+	if (input == NULL) printf("Error: Can't open file \"%s\"\n", FileName);
 	else
 	{
-		fread (&FileHeader, 1, 14, input);
-		fread (&InfoHeader, 1, 14, input);
+		fread (&Header, 1, 0x36, input);
 		//fread (Image, 1, 14, input);
 		fclose (input);
 	}
@@ -62,15 +68,52 @@ BMP::~BMP ()
 }
 
 
-BMP::ImageInfo ()
+void BMP::ViewImageInfo ()
 {
-	printf("%c%c\n", char Type, );
+	printf(	"Type            = %x\n"
+			"FileSize        = %x\n"
+			"Reserved1       = %x\n"
+			"Reserved2       = %x\n"
+			"OffsetBits      = %x\n"
+			"InfoHeaderSize  = %x\n"
+			"Width           = %x\n"
+			"Height          = %x\n"
+			"Planes          = %x\n"
+			"BitCount        = %x\n"
+			"Compression     = %x\n"
+			"SizeImage       = %x\n"
+			"XpxlsPerMeter   = %x\n"
+			"YpxlsPerMeter   = %x\n"
+			"ColorsUsed      = %x\n"
+			"ColorsImportant = %x\n",
+			//*((char *) &Header.Type),
+			//*(((char *) &Header.Type) + 1),
+			Header.Type,
+			Header.FileSize,
+			Header.Reserved1,
+			Header.Reserved2,
+			Header.OffsetBits,
+			Header.InfoHeaderSize,
+			Header.Width,
+			Header.Height,
+			Header.Planes,
+			Header.BitCount,
+			Header.Compression,
+			Header.SizeImage,
+			Header.XpxlsPerMeter,
+			Header.YpxlsPerMeter,
+			Header.ColorsUsed,
+			Header.ColorsImportant);
+
+	return;
 }
 
 
 int main ()
 {
+	BMP Cat (Input_file);
 
+	Cat.ViewImageInfo ();
 
 	return 0;
 }
